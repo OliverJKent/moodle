1d92f0e5cd7f6609a7f7624ab76517cae8b30458
OliverJKent <42536855+OliverJKent@users.noreply.github.com>
2019-05-13 16:50:11 +0100

    Remove all HTML tags except those specified

--- backup/moodle2/restore_stepslib.php
+++ backup/moodle2/restore_stepslib.php
@@ -1611,7 +1611,8 @@ class restore_section_structure_step extends restore_structure_step {
             // whether existing or new one should take precedence).
             
             //SSU_AMEND START Remove styling from the HTML during course imports
-            $data->intro = preg_replace('/(<[^>]*) style=("[^"]+"|\'[^\']+\')([^>]*>)/i', '$1$3', $data->intro);
+            $section->summary = strip_tags($data->intro, '<p><br><ul><ol><li>'); //Remove all HTML tags execpt those specified
+            $section->summary = preg_replace('/(<[^>]*) style=("[^"]+"|\'[^\']+\')([^>]*>)/i', '$1$3', $section->summary); //Remove extra styling
             //SSU_AMEND END
 
             $DB->update_record('course_sections', $section);
